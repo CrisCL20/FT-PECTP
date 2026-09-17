ae888d03a4d7fea61f50602d2d0b9ad4826ac797
csoriano <csoriano@alumnos.inf.utfsm.cl>
2026-09-17 18:03:30 -0300

    fix nsga pipe script

--- run_nsga_pipe.sh
+++ run_nsga_pipe.sh
@@ -17,5 +17,6 @@ pm_schedule=0.6
 top_tslots=5
 
 parallel -j ${workers} --bar \
-    "./nsga2r {1} ${INSTANCE_FILEPATH} ${popsize} ${gens} ${nobj} ${pc} ${pm} ${pm_ts_swap} ${pm_schedule} ${top_tslots} > logs/out_\$(basename ${INSTANCE_FILEPATH} .dat)_s{1}.log 2>&1" \
-    ::: "${seeds[@]}" 
+    "./nsga2r {1} {2} ${popsize} ${gens} ${nobj} ${pc} ${pm} ${pm_ts_swap} ${pm_schedule} ${top_tslots} > logs/out_\$(basename {2} .dat)_s{1}.log 2>&1" \
+    ::: "${seeds[@]}" \
+    ::: "${INSTANCE_FILEPATH}"
